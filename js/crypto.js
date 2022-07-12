ee7a6ad765055ada3cc545986a5ce34af8c44080
Kevin Mauel <kevin.mauel2+github@gmail.com>
2022-07-12 13:29:23 +0200

    feat: add minting functionality

--- js/crypto.js
+++ js/crypto.js
@@ -20,7 +20,7 @@ const etherscan = {
 const contractAddress = {
     mainnet: '',
     ropsten: '',
-    rinkeby: ''
+    rinkeby: '0x9e020BC81597d27A1c3EF044AA331943082B6AF3'
 };
 const Web3Modal = window.Web3Modal.default;
 const WalletConnectProvider = window.WalletConnectProvider.default;
@@ -33,6 +33,9 @@ let EthersSigner;
 let Web3Provider;
 let web3Modal;
 let connectedWallet;
+let saleState;
+let maxMintAmount = 0;
+let mintedAmount = 0;
 
 const parseError = function (error) {
     if (typeof error === 'string') {
@@ -60,7 +63,7 @@ window.addEventListener('load', async function() {
     modal.show();*/
 
     EthersProvider.pollingInterval = 1000 * 240; // 4min
-    //ReadingContract = new ethers.Contract(contractAddress[network], await (await fetch('abi_'+network+'.json')).json(), EthersProvider);
+    ReadingContract = new ethers.Contract(contractAddress[network], await (await fetch('abi_'+network+'.json')).json(), EthersProvider);
 
     web3Modal = new Web3Modal({
         cacheProvider: false,
@@ -86,6 +89,33 @@ window.addEventListener('load', async function() {
         disableInjectedProvider: false
     });
 
+    const refresh = async function () {
+        if (EthersSigner === undefined) {
+            throw new Error('Not connected');
+        }
+
+        const address = await EthersSigner.getAddress();
+
+        saleState = await ReadingContract.saleState();
+
+        if (saleState >= 2) {
+            maxMintAmount = (await ReadingContract.maxMintPerWallet()).toNumber();
+            mintedAmount = (await ReadingContract.publicAddressesMinted(address)).toNumber();
+        }
+
+        if (saleState === 0) {
+            maxMintAmount = (await ReadingContract.genesisAddresses(address)).toNumber();
+            mintedAmount = (await ReadingContract.genesisAddressesMinted(address)).toNumber();
+        }
+
+        if (saleState === 1) {
+            maxMintAmount = (await ReadingContract.whitelistAddresses(address)).toNumber();
+            mintedAmount = (await ReadingContract.whitelistAddressesMinted(address)).toNumber();
+        }
+
+        console.log(address, saleState, maxMintAmount, mintedAmount);
+    }
+
     const onConnect = async function () {
         try {
             Web3Provider = await web3Modal.connect();
@@ -100,7 +130,26 @@ window.addEventListener('load', async function() {
             EthersSigner = (new ethers.providers.Web3Provider(Web3Provider)).getSigner();
 
             connectedWallet = ethers.utils.getAddress(Web3Provider.selectedAddress);
-            this.querySelector('.connect-button-inner').innerText = formatWalletAddress(connectedWallet);
+
+            await refresh();
+
+            if (saleState === 3) {
+                this.querySelector('.connect-button-inner').innerText = formatWalletAddress(connectedWallet);
+            } else {
+                document.getElementById('mint-amount-container').classList.remove('hide');
+                this.querySelector('.connect-button-inner').innerText = 'Mint';
+
+                for (const connectButton of document.getElementsByClassName('connect-button')) {
+                    connectButton.removeEventListener('click', onConnect, false);
+                }
+
+                this.classList.remove('connect-button');
+                this.classList.add('mint-button');
+
+                for (const mintButton of document.getElementsByClassName('mint-button')) {
+                    mintButton.addEventListener('click', onMint);
+                }
+            }
         } catch (error) {
             console.error(error);
             return;
@@ -117,7 +166,73 @@ window.addEventListener('load', async function() {
         });
     };
 
+    const onMint = async function () {
+        if (EthersSigner === undefined) {
+            throw new Error('Not connected');
+        }
+
+        const Contract = ReadingContract.connect(EthersSigner);
+
+        const walletBalance = await EthersSigner.getBalance();
+
+        let mintPrice = '0.1';
+        if (saleState === 0) {
+            mintPrice = '0.05';
+        }
+        if (saleState === 1) {
+            mintPrice = '0.075';
+        }
+
+        const mintAmount = parseInt(document.getElementById('mint-amount').innerText);
+        if (mintAmount === 0) {
+            throw new Error('Cannot mint zero NFTs');
+        }
+        const mintValue = ethers.utils.parseEther(mintPrice).mul(mintAmount);
+
+        if (mintValue.gte(walletBalance)) {
+            throw new Error(`Not enough funds to mint ${mintAmount} for ${ethers.utils.formatEther(mintValue)}`);
+        }
+
+        const mintTx = await Contract.mint(mintAmount, {value: mintValue});
+
+        const receiptCall = mintTx.wait(1);
+
+        receiptCall.catch(function (error) {
+            console.error(error);
+        });
+
+        receiptCall.then(async function (receipt) {
+            console.debug('Mint successful', receipt);
+
+            await refresh();
+        });
+    }
+
     for (const connectButton of document.getElementsByClassName('connect-button')) {
         connectButton.addEventListener('click', onConnect);
     }
+
+    document.getElementById('increment-mint').addEventListener('click', function () {
+        let counter = document.getElementById('mint-amount');
+
+        const incrementedAmount = parseInt(counter.innerText) + 1;
+
+        if (incrementedAmount > maxMintAmount - mintedAmount) {
+            return;
+        }
+
+        counter.innerText = incrementedAmount;
+    });
+
+    document.getElementById('decrement-mint').addEventListener('click', function () {
+        let counter = document.getElementById('mint-amount');
+
+        const decrementedAmount = parseInt(counter.innerText) - 1;
+
+        if (decrementedAmount < 0) {
+            return;
+        }
+
+        counter.innerText = decrementedAmount;
+    });
 });
